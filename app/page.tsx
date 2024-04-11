da7a6d2796fcc03a921659cb180e7818d07df113
Diego <moctezumarafael26@gmail.com>
2024-04-11 15:14:39 -0600

    hola

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ import Carta from "@/components/carta/Carta"
 export default function page() {
   return (
     <>
+      <div className="d-flex">
       <Carta imagen="https://assets.pokemon.com/assets/cms2/img/pokedex/full/613.png" titulo="POKEMON" descripcion="Tipo hielo"/>
       <Carta imagen="https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png" titulo="Cyndaquil" descripcion="Tipo fuego"/>
       <Carta imagen="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" titulo="Pikachu"  descripcion="Tipo electrico"/>
+      </div>
     </>
 
   )
